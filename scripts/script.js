945c4d54158b411ff30e5fe51c695ea750345a14
Moitrayee Choudhury <moitrayee3009@gmail.com>
2018-11-09 19:42:09 +0100

    info page updated

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,16 @@ let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 let btnGetStarted = document.getElementById ('btnGetStarted');
 
+// EventListener for hamburger menu.
+
 navBarToggle.addEventListener('click', function () {
     
     mainNav.classList.toggle('active');
 });
 
+// EventListener for opening editor.
+
 btnGetStarted.addEventListener('click', function() {
-    window.open("editor.html");
+    // window.open("editor.html");
+    window.open('editor.html','_self');
 });
