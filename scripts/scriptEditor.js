106997554dff9640c082e61a34207b8952426538
phiribribera <philip.ribera@student.kyh.se>
2018-11-15 15:28:11 +0100

    style changes to editor and scripts for nav menu

--- scripts/scriptEditor.js
+++ scripts/scriptEditor.js
@@ -6,13 +6,27 @@
 // ANCHORS 
 var menuToggler = document.getElementById('menu-toggler');
 var collapsedMenu = document.getElementById("collapse-target");
+var navNewNote = document.getElementById('navNewNote');
+var navSearch = document.getElementById('navSearch');
+var noteTitle = document.getElementById('noteTitle');
+var noteSearch = document.getElementById('noteSearch');
 
 
 // EVENT LISTENERS 
 menuToggler.addEventListener('click', toggleMenu);
-
+navNewNote.addEventListener('click', focusOnTitle);
+navSearch.addEventListener('click', focusOnSearch);
 
 // CALLBACKS
+
+function focusOnTitle() {
+    noteTitle.focus();  
+}
+
+function focusOnSearch() {
+    noteSearch.focus();    
+}
+
 
 // Burger menu - small screens
 function toggleMenu() {
