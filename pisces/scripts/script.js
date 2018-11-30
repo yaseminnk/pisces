04884675a7a1f275ea0c0a8ba131a673e900148d
Dessi Costa <desiree.costa@student.kyh.se>
2018-11-30 14:44:03 +0100

    new files

---
+++ pisces/scripts/script.js
@@ -0,0 +1,31 @@
+
+let mainNav = document.getElementById('jsMenu');
+let navBarToggle = document.getElementById('jsNavbarToggle');
+let btnGetStarted = document.getElementById ('btnGetStarted');
+
+
+// EventListener for hamburger menu.
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('active');
+});
+
+// EventListener for opening editor.
+btnGetStarted.addEventListener('click', function() {    
+    window.open('editor.html','_self');
+});
+
+
+// check if there are cookies and if so redirect the user to the editor
+/*
+var values = document.cookie;
+
+if( values.indexOf("visitedQuire=") === -1 ) {
+    document.cookie = "visitedQuire=true;"; // setting cookie
+    //window.open('index.html', '_self');        
+ 
+ } else {
+    //document.cookie = "visitedQuire=false"; // setting cookie
+    window.open('editor.html', '_self');
+ }
+ */
