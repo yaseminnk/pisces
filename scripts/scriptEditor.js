f78d0d293e03f9982be1c661a9aea59118551b93
phiribribera <philip.ribera@student.kyh.se>
2018-12-03 20:36:43 +0100

    Added theme selection to the editor with logik included

--- scripts/scriptEditor.js
+++ scripts/scriptEditor.js
@@ -84,6 +84,7 @@ var blue = document.getElementById('blue');
 var pink = document.getElementById('pink');
 
 // EVENT LISTENERS
+/*
 stand.addEventListener('click', switchTheme); 
 green.addEventListener('click', switchTheme); 
 blue.addEventListener('click', switchTheme); 
@@ -93,7 +94,7 @@ function switchTheme() {
     themeField.setAttribute('class', ''); 
     themeField.classList.toggle(this.id);   
 }
-
+*/
 //dropdown menu
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
@@ -113,3 +114,33 @@ for (i = 0; i < dropdown.length; i++) {
 function delete_cookie( name ) {
     document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
   }
+
+
+/*** DROPDOWN MENU THEME SELECTION ***/
+
+    // ANCHOR
+    var btnDrop = document.querySelector('.btn-drop');
+
+    
+    // LISTENERS
+    btnDrop.addEventListener('click', showDropTheme);
+
+
+    // CALLBACKS
+    function showDropTheme() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function (event) {
+        if (!event.target.matches('.btn-drop')) {
+
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
