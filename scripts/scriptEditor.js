536b5e9391d4b6a3917d48dcb5cb6f896707b0d6
Dessi Costa <desiree.costa@student.kyh.se>
2018-12-06 12:45:49 +0100

    new files

--- scripts/scriptEditor.js
+++ scripts/scriptEditor.js
@@ -3,11 +3,6 @@
 /**************************/
 
 // NEW SCOPE ***//
-<<<<<<< HEAD
-
-=======
-{
->>>>>>> 2a3af3142a486d2f141587ec5014732eab8f69c7
     // ANCHORS 
     let logo = document.getElementById('logo');
     let logoShort = document.getElementById('logoShortVer');
@@ -93,7 +88,6 @@
             cM.classList.toggle('menu-collapse');
         }
     }
-<<<<<<< HEAD
 
     // Field for my notes and favorite notes
     function showNotesField() {
@@ -121,92 +115,6 @@
 /*** NEW SCOPE ***/
 {
     // ANCHORS
-    let qlContainer = document.querySelector('.ql-toolbar');
-    let elSpan = document.createElement('span');
-    let elAtag = document.createElement('a');
-    let elIcon = document.createElement('i');
-
-    /*** We add a print icon to the toolbar ***/    
-    elSpan.setAttribute('class', 'ql-formats');
-    elIcon.setAttribute('class', 'fas fa-print');
-    elIcon.classList.add('toolbar-print-icon');
-    elAtag.setAttribute('class', 'print-note');
-    elAtag.setAttribute('title', 'Print note');
-    elAtag.id = "toolbarPrintNote";
-    // We append all childs
-    elAtag.appendChild(elIcon);
-    elSpan.appendChild(elAtag);
-    qlContainer.appendChild(elSpan);
-
-    let toolPrint = document.getElementById('toolbarPrintNote');
-    let btnPrint = document.getElementById('printNote');
-    let btnPrintHd = document.getElementById('printNoteHd');
-
-    toolPrint.addEventListener('click', printContent);
-    btnPrint.addEventListener('click', printContent);
-    btnPrintHd.addEventListener('click', printContent);
-
-    // CALLBACK PRINT FUNCTION
-    function printContent() {
-        window.print();
-    }
-
-    function delete_cookie(name) {
-        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    }
-}
-
-
-=======
-
-    // Field for my notes and favorite notes
-    function showNotesField() {
-        notesContainer.classList.toggle('data-container-hd');
-    }
-
-    //dropdown menu
-    var dropdown = document.getElementsByClassName("dropdown-btn");
-    var i;
-
-    for (i = 0; i < dropdown.length; i++) {
-        dropdown[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            var dropdownContent = this.nextElementSibling;
-            if (dropdownContent.style.display === "block") {
-                dropdownContent.style.display = "none";
-            } else {
-                dropdownContent.style.display = "block";
-                navBar.style.padding = "0 22px";
-            }
-        });
-    }
-}
->>>>>>> 2a3af3142a486d2f141587ec5014732eab8f69c7
-
-/*** NEW SCOPE ***/
-{
-    // ANCHORS
-<<<<<<< HEAD
-    let btnDrop = document.querySelector('.btn-drop');
-    let tmpDef = document.getElementById('tmpDefault');
-    let tmpBus = document.getElementById('tmpBusiness');
-    let tmpParty = document.getElementById('tmpParty');
-    let tmpFan = document.getElementById('tmpFantasy');
-    let tmpPlay = document.getElementById('tmpPlayful');
-    let dynStyle = document.getElementById('dynamicStylesheet');
-
-
-
-    // LISTENERS
-    btnDrop.addEventListener('click', showDropTheme);
-    tmpDef.addEventListener('click', changeTemplate);
-    tmpBus.addEventListener('click', changeTemplate);
-    tmpParty.addEventListener('click', changeTemplate);
-    tmpFan.addEventListener('click', changeTemplate);
-    tmpPlay.addEventListener('click', changeTemplate);
-
-
-=======
     let qlContainer = document.querySelector('.ql-toolbar');
     let elSpan = document.createElement('span');
     let elAtag = document.createElement('a');
@@ -252,7 +160,10 @@
     let tmpBus = document.getElementById('tmpBusiness');
     let tmpParty = document.getElementById('tmpParty');
     let tmpFan = document.getElementById('tmpFantasy');
+    let tmpPlay = document.getElementById('tmpPlayful');
     let dynStyle = document.getElementById('dynamicStylesheet');
+
+
 
     // LISTENERS
     btnDrop.addEventListener('click', showDropTheme);
@@ -260,7 +171,9 @@
     tmpBus.addEventListener('click', changeTemplate);
     tmpParty.addEventListener('click', changeTemplate);
     tmpFan.addEventListener('click', changeTemplate);
->>>>>>> 2a3af3142a486d2f141587ec5014732eab8f69c7
+    tmpPlay.addEventListener('click', changeTemplate);
+
+
 
     // CALLBACKS
     
@@ -282,38 +195,10 @@
             }
         }
     }
-
-<<<<<<< HEAD
-
-
-=======
-    // CALLBACKS
-    
-    /*** DROPDOWN MENU TEMPLATE SELECTION ***/
-    function showDropTheme() {
-        document.getElementById("myDropdown").classList.toggle("show");
-    }
-    // Close the dropdown menu if the user clicks outside of it
-    window.onclick = function (event) {
-        if (!event.target.matches('.btn-drop')) {
-
-            var dropdowns = document.getElementsByClassName("dropdown-content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-            }
-        }
-    }
-
-    /*** Change template - change dynamic stylesheet ***/
 
     function changeTemplate() {
         dynStyle.setAttribute('href', '');
         var elId = this.id;
->>>>>>> 2a3af3142a486d2f141587ec5014732eab8f69c7
 
         if (elId === 'tmpDeafult') {
             dynStyle.setAttribute('href', '#');
@@ -321,16 +206,22 @@
         else if (elId === 'tmpBusiness') {
             dynStyle.setAttribute('href', 'css/stylesheetBusiness.css');
         }
+        else if (elId === 'tmpParty') {
+            dynStyle.setAttribute('href', 'css/stylesheetParty.css');
+        }
+        else if (elId === 'tmpFantasy') {
+            dynStyle.setAttribute('href', 'css/stylesheetFantasy.css');
+        }
+        else if (elId === 'tmpPlayful') {
+            dynStyle.setAttribute('href', 'css/stylesheetPlayful.css');
+        }
     }
-<<<<<<< HEAD
-
-
-
-=======
 }
 
 
->>>>>>> 2a3af3142a486d2f141587ec5014732eab8f69c7
+
+
+
 /*** NEW SCOPE ***/
 {
     // ANCHORS
@@ -339,11 +230,13 @@
     let sky = document.getElementById('sky');
     let themeSet = document.getElementById('themeFieldset');
     let btnDrop = document.querySelector('.btn-drop');
+    let favContainer = document.querySelector('.note-fav-container');
     let logoBrand = document.getElementById('logoBrand');
 
     // EVENT LISTENERS
     earth.addEventListener('click', changeStylesheet);
     sky.addEventListener('click', changeStylesheet);
+  
 
     // CALLBACKS
 
@@ -360,6 +253,7 @@
         }
         else if (elId === 'sky') {
             menuField.setAttribute('class', 'skyBg');
+            favContainer.style.backgroundColor = "rgb(182,182,182)";
             themeSet.style.borderColor = "rgb(182,182,182)";
             btnDrop.style.backgroundColor = "rgb(23, 117, 171)";
             logoBrand.style.backgroundColor = "rgb(16, 113, 167)";
@@ -384,7 +278,6 @@ Quill.register('modules/counter', function (quill, options) {
 var quill = new Quill('#editor', {
     modules: {
         counter: true
-<<<<<<< HEAD
     }
     else if (elId === 'tmpParty'){
         dynStyle.setAttribute('href', 'css/stylesheetParty.css');
@@ -399,8 +292,3 @@ var quill = new Quill('#editor', {
      
 
 
-=======
-    }
-});
-*/
->>>>>>> 2a3af3142a486d2f141587ec5014732eab8f69c7
